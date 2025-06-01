f495baf0c77165b3fad3babebff0db6ef32507b1
shivam-wlc <shivam@wlc.technology>
2025-06-01 10:42:24 +0530

    auth

--- client/src/allRoutes/AllRoutes.jsx
+++ client/src/allRoutes/AllRoutes.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
+import Register from "../pages/Register";
+import Login from "../pages/Login";
 
 const AllRoutes = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Register />} />
+        <Route path="/register" element={<Login />} />
       </Routes>
     </>
   );
